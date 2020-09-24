6639b1a8c6fbb38c9d4abc87fb1bedc7fbd6d7ee
icaronascimento2012 <icaronascimento2012@gmail.com>
2020-09-24 19:14:56 -0400

    update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "O meu mundo interior cria o meu mundo exterior, eu tenho uma mente milionaria")
+        self.assertRegex(result.data.decode(), "teste")
 
 
 if __name__ == "__main__":
